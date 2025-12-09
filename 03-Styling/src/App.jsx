aee989d7c67516e78ee5dd5a7bdfb957b07daa41
aditya <adityarai2112000@gmail.com>
2025-12-09 17:56:58 +0530

    this is update till styling

---
+++ 03-Styling/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Button from "./components/Button/Button";
+
+const App = () => {
+  return <div className="h-screen w-full bg-gray-600"></div>;
+};
+
+export default App;
---
+++ 03-Styling/src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = () => {
+  return <div className={styles.btn}>this is button component</div>;
+};
+
+export default Button;
